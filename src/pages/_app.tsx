043af80041c3972039fd5e705a47c624180bb111
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2023-01-20 00:24:56 -0500

    feat: add navigation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import UserProvider from "../context/user";
+import UserProvider from "@/context/user";
+import Nav from "@/components/nav";
 import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <main className={inter.className}>
+        <Nav />
         <Component {...pageProps} />
       </main>
     </UserProvider>
